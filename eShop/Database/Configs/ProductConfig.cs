d33e9c5e7464878446304b710d1c2e59d0194a12
TienSi <ntt18082001@gmail.com>
2022-08-23 19:49:44 +0700

    First commit

---
+++ eShop/Database/Configs/ProductConfig.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShop.Database.Configs
+{
+	public class ProductConfig : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.Property(x => x.Name)
+				.HasMaxLength(200);
+			builder.Property(x => x.Description)
+				.HasColumnType("ntext");
+			builder.Property(x => x.CoverImg)
+				.HasMaxLength(500);
+
+			// Khóa ngoại
+			builder.HasOne(x => x.ProductCategory)
+				.WithMany()
+				.IsRequired(false)
+				.HasForeignKey(x => x.CategoryId);
+		}
+	}
+}
